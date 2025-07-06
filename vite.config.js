eaac9d47e4c585b24f0b82e4cd5af25dd7362c3a
lakshmi364 <kurmioppu@gmail.com>
2025-07-07 03:39:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-  define: {
-    'process.env': {},
   },
   build: {
-    outDir: 'dist', // Netlify will use this directory for publishing
+    outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: './index.html', // ensures Netlify knows what the entry is (required if index.html is used)
-    },
   },
-});
+})
