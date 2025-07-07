3d4da349fff538127b6f5b65a93a9243d8bae908
lakshmi364 <kurmioppu@gmail.com>
2025-07-08 02:40:25 +0530

    Update input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,10 +1,1 @@
-import React from 'react';
 
-export const Input = ({ className = '', ...props }) => {
-  return (
-    <input
-      className={`px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-500 ${className}`}
-      {...props}
-    />
-  );
-};
