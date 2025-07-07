a42f3976655e50ce9057b0b03117c36040a11b62
lakshmi364 <kurmioppu@gmail.com>
2025-07-07 13:14:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,7 +10,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
