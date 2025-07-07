04e2262ec3577dd89125c2a53ad1290e18bddf17
lakshmi364 <kurmioppu@gmail.com>
2025-07-07 21:10:52 +0530

    Create input.jsx

---
+++ src/components/ui/input.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Input = React.forwardRef(({ className = '', type = 'text', ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      type={type}
+      className={`w-full px-4 py-2 border border-pink-300 rounded-xl shadow-sm bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-rose-400 transition duration-200 ${className}`}
+      {...props}
+    />
+  );
+});
+
+Input.displayName = 'Input';
+
+export default Input
