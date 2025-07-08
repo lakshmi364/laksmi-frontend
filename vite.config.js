4ea03cb1c0f918efc13d6037c07585bbaa55cebb
lakshmi364 <kurmioppu@gmail.com>
2025-07-08 13:50:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
   server: {
-    port: 5173
-  }
-});
+    port: 5173,
+  },
+})
